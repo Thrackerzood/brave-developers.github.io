844b8cb3295b8d528c1e0f3e1ff601ec386cb7fe
Thrackerzood <71223383+Thrackerzood@users.noreply.github.com>
2021-08-14 17:12:25 +0400

    Update 1 - task.js

--- 1 - task.js
+++ 1 - task.js
@@ -1,8 +1,6 @@
-function search(str, a, b){
-      if (str === '') return -1
-      const num1 = [...str].indexOf(a)
-      const num2 = [...str].indexOf(b)
-      return ( num1 >= num2 ) ? num1 : num2
-   }
-      
-   console.log(search('return2This3','2','2'))
+function search(str:string, a:string, b:string){
+      const num1 = str.lastIndexOf(a)
+      const num2 = str.lastIndexOf(b)
+      return str === '' ? -1 : (num1 > num2 ? num2 : num1)
+}
+console.log(search('return2This3','r','s'))
